8d9775073909a3f4e66849f202d2bd5d3c99a05a
zachnewburgh <zach.newburgh@gmail.com>
2016-10-28 11:45:36 -0400

    Created the Project factory

---
+++ app/assets/javascripts/angular/models/project.js
@@ -0,0 +1,10 @@
+function Project($resource) {
+  var Project = $resource('http://localhost:3000/api/v1/projects/:id.json', {id: '@id'}, {
+    update: {method: 'PUT'}
+  });
+  return Project;
+};
+
+angular
+  .module('app')
+  .factory('Project', Project);
