22530b7d9eff118122e21a14852e0b6cbf45bb6d
zachnewburgh <zach.newburgh@gmail.com>
2016-10-28 11:38:37 -0400

    Created ProjectsController wth CRUD actions

---
+++ app/controllers/api/v1/projects_controller.rb
@@ -0,0 +1,45 @@
+module Api
+  module V1 
+    class ProjectsController < ApplicationController 
+      skip_before_filter :verify_authenticity_token 
+      respond_to :json 
+      
+      def index 
+        respond_with(Project.all.order("id DESC"))
+      end 
+      
+      def show 
+        respond_with(Project.find(params[:id]))
+      end 
+      
+      def create 
+        @project = Project.new(project_params) 
+        if @project.save 
+          respond_to do |format|
+            format.json { render json: @project }
+          end 
+        end 
+      end 
+      
+      def update 
+        @project = Project.find(params[:id])
+        if @project.update(project_params) 
+          respond_to do |format| 
+            format.json { render json: @project }
+          end 
+        end 
+      end 
+ 
+      def destroy 
+        respond_with Project.destroy(params[:id]) 
+      end 
+      
+      private
+      
+      def project_params 
+        params.require(:project).permit(:[attribute1], :[attribute2]) 
+      end 
+      
+    end 
+  end
+end
