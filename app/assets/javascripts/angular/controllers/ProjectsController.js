40fe8554ff7e0da521a3d2d5af9435fac5acda70
zachnewburgh <zach.newburgh@gmail.com>
2016-10-28 12:58:09 -0400

    Finished setting up CRUD functionality for projects

--- app/assets/javascripts/angular/controllers/ProjectsController.js
+++ app/assets/javascripts/angular/controllers/ProjectsController.js
@@ -1,13 +1,26 @@
-function ProjectsController(Project, $location, $state) {
+function ProjectsController(Project, $location, $state, $stateParams) {
 
   var ctrl = this;
   
   ctrl.projects = Project.query();
-  ctrl.project = new Project();
+  ctrl.projectNew = new Project();
+  ctrl.project = Project.get({id: $stateParams.id});
 
   ctrl.addProject = function() {
-    ctrl.project.$save(function() {
+    ctrl.projectNew.$save(function() {
       $location.path('projects');
+    });
+  };
+
+  ctrl.editProject = function() {
+    ctrl.project.$update(function() {
+      $location.path('projects');
+    });
+  };
+
+  ctrl.deleteProject = function(project) {
+    project.$delete(function() {
+      $state.reload();
     });
   };
 
