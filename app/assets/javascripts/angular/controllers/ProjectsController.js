5fd5aff484e7b7ac02f9afc85a8ed875abf4092e
zachnewburgh <zach.newburgh@gmail.com>
2016-10-28 12:43:12 -0400

    Added new template, and ctrl.project and ctrl.addProject to controller

--- app/assets/javascripts/angular/controllers/ProjectsController.js
+++ app/assets/javascripts/angular/controllers/ProjectsController.js
@@ -1,7 +1,15 @@
 function ProjectsController(Project, $location, $state) {
 
   var ctrl = this;
+  
   ctrl.projects = Project.query();
+  ctrl.project = new Project();
+
+  ctrl.addProject = function() {
+    ctrl.project.$save(function() {
+      $location.path('projects');
+    });
+  };
 
 };
 
