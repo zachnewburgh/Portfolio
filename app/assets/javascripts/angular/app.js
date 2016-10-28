379b8c44c9325b0da440cdde08e87943d5bc900d
zachnewburgh <zach.newburgh@gmail.com>
2016-10-28 12:14:52 -0400

    Defined projects states

--- app/assets/javascripts/angular/app.js
+++ app/assets/javascripts/angular/app.js
@@ -1,2 +1,30 @@
 angular
   .module('app', ['ui.router', 'ngResource']) 
+  .config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('projects', {
+        templateUrl: 'navbar/navbar.html',
+        controller: 'ProjectsController as ctrl'
+      })
+      .state('projects.index', {
+        url: '/',
+        templateUrl: 'projects/index.html',
+        controller: 'ProjectsController as ctrl'
+      })
+      .state('projects.new', {
+        url: 'projects/new',
+        templateUrl: 'projects/new.html',
+        controller: 'ProjectsController as ctrl'
+      })
+      .state('projects.show', {
+        url: 'projects/:id',
+        templateUrl: 'projects/show.html',
+        controller: 'ProjectsController as ctrl'
+      })
+      .state('projects.edit', {
+        url: 'projects/:id/edit',
+        templateUrl: 'projects/edit.html',
+        controller: 'ProjectsController as ctrl'
+      });
+    $urlRouterProvider.otherwise('/');
+});
