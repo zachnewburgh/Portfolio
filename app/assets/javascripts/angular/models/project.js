34f706d17215ed3fce33db4163d6918993331db8
zachnewburgh <zach.newburgh@gmail.com>
2016-10-30 23:34:14 -0400

    Changed get to query and assigned true to isArray

--- app/assets/javascripts/angular/models/project.js
+++ app/assets/javascripts/angular/models/project.js
@@ -1,7 +1,7 @@
 Project = ['$resource', function($resource) {
   var Project = $resource(
     'api/v1/projects/:id.json', {id: '@id'}, {
-    get: {method: 'GET', isArray: false},
+    query: {method: 'GET', isArray: true},
     update: {method: 'PUT', isArray: false}
   });
   return Project;
