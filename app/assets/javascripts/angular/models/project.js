4e2b4c4e51e456531ba2de9d3824b154940db728
zachnewburgh <zach.newburgh@gmail.com>
2016-10-30 23:11:36 -0400

    Preventing minification by injecting dependencies

--- app/assets/javascripts/angular/models/project.js
+++ app/assets/javascripts/angular/models/project.js
@@ -1,9 +1,9 @@
-function Project($resource) {
+Project = ['$resource', function($resource) {
   var Project = $resource('http://localhost:3000/api/v1/projects/:id.json', {id: '@id'}, {
     update: {method: 'PUT'}
   });
   return Project;
-};
+}];
 
 angular
   .module('app')
